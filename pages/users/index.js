bb6f9496c4ee4193e3c59593317669e0287be96e
SabbirSami <smd71430@gmail.com>
2022-06-01 11:45:25 +0600

    fetch data using function getStaticProps

--- pages/users/index.js
+++ pages/users/index.js
@@ -1,11 +1,18 @@
 import React from "react";
 
-const index = () => {
+const index = ({ users }) => {
     return (
         <div>
-            <h2>This is user main page</h2>
+            <h2>This is user main page : {users.length}</h2>
         </div>
     );
 };
 
 export default index;
+export async function getStaticProps(context) {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await res.json();
+    return {
+        props: { users: data }, // will be passed to the page component as props
+    };
+}
