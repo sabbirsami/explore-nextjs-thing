798a260a8d511675ab16ec07a86b83aad29f343c
SabbirSami <smd71430@gmail.com>
2022-06-01 12:24:49 +0600

    SSR overview and dynamic route generation

--- pages/users/index.js
+++ pages/users/index.js
@@ -1,9 +1,16 @@
+import Link from "next/link";
 import React from "react";
 
 const index = ({ users }) => {
     return (
         <div>
             <h2>This is user main page : {users.length}</h2>
+            {users.map((user) => (
+                <div key={user.id}>
+                    <h3>Name: {user.name}</h3>
+                    <Link href={`/users/${user.id}`}>Explore</Link>
+                </div>
+            ))}
         </div>
     );
 };
